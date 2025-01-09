e9e8e0552b1748f68b8a7542f286319d2c795096
Numinax <73579300+YJbrd@users.noreply.github.com>
2025-01-09 10:08:35 +0100

    mise à jour liée à la base

--- Server Database/Wathaleak_Server_Data_processing_SQL.py
+++ Server Database/Wathaleak_Server_Data_processing_SQL.py
@@ -7,36 +7,36 @@ import time
 def save_data(node_id, mesure, status):
     conn = sqlite3.connect("wal_database.db")
     cursor = conn.cursor()
-    timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
-    cursor.execute("INSERT INTO Datas (TimeStamp, NodeID, MesureCapteur, Status) VALUES (?, ?, ?, ?)", 
-                   (timestamp, node_id, mesure, status))
+    cursor.execute("INSERT INTO Data (NodeID, MesureCapteur, Status, TimeStamp, Batterie) VALUES (?, ?, ?, ?, ?)", 
+                   (node_id, mesure, status, timestamp, batterie))
     conn.commit()
     conn.close()
 
 # Attribue un statut en fonction de la valeur
 def give_status(value):
     if value < 10:
-        return "Critical"
-    elif value < 20:
-        return "Bad"
+        return 0
     else:
-        return "Good"
+        return 1
 
 # Réception des messages MQTT
 def on_message(client, userdata, message):
-    data = json.loads(message.payload.decode())
-    node_id = data["node_id"]
-    mesure = data["value"]
+     data = json.loads(message.payload.decode())
+     node_id = data["node_id"]
+     mesure = data["value"]
+     status = give_status(mesure)
+     timestamp = data["timestamp"]
+     batterie = data["status"]     
 
     # Attribue un statut et sauvegarde
-    status = give_status(mesure)
-    save_data(node_id, mesure, status)
+     print("message recu !!!" + timestamp)
+     save_data(node_id, mesure, status,timestamp,batterie)
 
 # Configuration MQTT
 MQTT_BROKER = "localhost"  
 MQTT_PORT = 1883
-MQTT_USERNAME = "username"  
-MQTT_PASSWORD = "mdp"  
+MQTT_USERNAME = "wal"  
+MQTT_PASSWORD = "walwal"  
 MQTT_TOPIC = "sensors/data"
 
 # Client MQTT
