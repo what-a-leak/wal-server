d82ef60ab311a46f92fe1ea8757489f1b5fd42a1
Numinax <73579300+YJbrd@users.noreply.github.com>
2025-01-15 17:19:09 +0100

    Ajout de la temperature et update du json

--- Server Database/Whataleak_Server_Data_processing_SQL.py
+++ Server Database/Whataleak_Server_Data_processing_SQL.py
@@ -1,68 +1,62 @@
 import sqlite3
 import paho.mqtt.client as mqtt
 import json
+import time
 
 # Enregistre les données dans la base
-def save_data(node_id, mesure, status):
+def save_data(node_id, mesure, status, timestamp, batterie, temperature):
     conn = sqlite3.connect("wal_database.db")
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO Data (NodeID, MesureCapteur, Status, TimeStamp, Batterie) VALUES (?, ?, ?, ?, ?)", 
-                   (node_id, mesure, status, timestamp, batterie))
+    cursor.execute("INSERT INTO Data (NodeID, MesureCapteur, Status, TimeStamp, Batterie, Temperature) VALUES (?, ?, ?, ?, ?, ?)", 
+                   (node_id, mesure, status, timestamp, batterie, temperature))
     conn.commit()
     conn.close()
+
+    generate_json_db()
 
 # Attribue un statut en fonction de la valeur
 def give_status(value):
     if value < 10:
-        return 0
+        return "0"
     else:
-        return 1
-    
-# Fonction generant le fichier JSON a partir de la table Datas
-def export_to_json():
+        return "1"
+
+##### AJOUT JSON TEst
+
+def generate_json_db():
     conn = sqlite3.connect("wal_database.db")
     cursor = conn.cursor()
-    
     cursor.execute("SELECT * FROM Data")
     rows = cursor.fetchall()
-    # Cree liste de dictionnaires JSON
-    data_list = []
-    for row in rows:
-        data_dict = {
-            "NodeID": row[0],
-            "MesureCapteur": row[1],
-            "Status": row[2],
-            "TimeStamp": row[3],
-            "Batterie": row[4]
-            
-        }
-        data_list.append(data_dict)
-    # Save dans fichier JSON
-    with open("sensor_data.json", "w") as json_file:
-        json.dump(data_list, json_file, indent=4)
-    
+
+    columns = [description[0] for description in cursor.description]
+    data = [dict(zip(columns, row)) for row in rows]
+
+    with open("data.json", "w") as json_file:
+        json.dump(data, json_file, indent=4)
+
     conn.close()
+
 
 # Réception des messages MQTT
 def on_message(client, userdata, message):
      data = json.loads(message.payload.decode())
      node_id = data["node_id"]
-     mesure = data["value"]
+     mesure = data["mesure"]
      status = give_status(mesure)
      timestamp = data["timestamp"]
-     batterie = data["status"]
-     
+     batterie = data["batterie"]
+     temperature = data["temperature"]     
 
-    # Attribue un statut et sauvegarde json
+    # Attribue un statut et sauvegarde
      print("message recu !!!" + timestamp)
-     save_data(node_id, mesure, status,timestamp,batterie)
-     export_to_json()
+     save_data(node_id, mesure, status, timestamp, batterie, temperature)
 
 # Configuration MQTT
 MQTT_BROKER = "localhost"  
 MQTT_PORT = 1883
-MQTT_USERNAME = "wal"  
-MQTT_PASSWORD = "walwal"  
+MQTT_USERNAME = ""  
+MQTT_PASSWORD = ""  
 MQTT_TOPIC = "sensors/data"
 
 # Client MQTT
