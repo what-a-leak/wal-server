2c05e89b19ae406387f34b4437684346a9aef77c
Numinax <73579300+YJbrd@users.noreply.github.com>
2025-01-15 17:16:19 +0100

    Ajout du programme de test de MQTT

---
+++ Server Database/mqtt_test.py
@@ -0,0 +1,30 @@
+import paho.mqtt.client as mqtt
+import json
+
+# Configuration
+MQTT_BROKER = "90.89.133.173"
+MQTT_PORT = 1883
+MQTT_USERNAME = "wal"
+MQTT_PASSWORD = "walwal"
+MQTT_TOPIC = "sensors/data"
+
+# Exemple de données
+sensor_data = {
+    "node_id": 1,
+    "mesure": 23.5,
+    "status": 1,
+    "timestamp": "2025-01-08 14:30:00",
+    "batterie": 90,
+    "temperature": 25
+}
+
+# Connexion au serveur MQTT
+client = mqtt.Client()
+client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+client.connect(MQTT_BROKER, MQTT_PORT)
+
+# Publication des données
+client.publish(MQTT_TOPIC, json.dumps(sensor_data))
+print("Données envoyées avec succès.")
+
+client.disconnect()
